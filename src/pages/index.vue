ddbbdaf1b35658c7c7e27d504b43af2a85a58775
scurrypf <2549018667@qq.com>
2023-02-27 17:42:40 +0800

    文章列表渲染初步

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,12 @@
 <script setup>
-import {onMounted} from 'vue'
-
-const article = await useFetch('/api/article');
-
-const display = article.data.value.summary;
-const tittle = article.data.value.tittle;
-const createdAt = article.data.value.createdAt;
-const src = `http://localhost:1337${article.data.value.img.url}`
-const good = article.data.value.good;
-const discuss = article.data.value.discuss;
-const see = article.data.value.see;
-const authorName = article.data.value.author.name;
-const tagArr = article.data.value.tags.data;
+import {onMounted , nextTick} from 'vue'
 
 const computeTime = function(createdAt){
   const created = new Date(createdAt);
   const now = new Date();
-  console.log(created,now);
-  const duration = (now.getTime() - created.getTime()) / 1000 / 60
+  // console.log(created,now);
+  const duration = (now.getTime() - created.getTime()) / 1000 / 60;
   let res = '刚刚';
-  
   if (duration > 0 && duration < 60){
     // 一小时内
     // 使用toFixed去掉时间后的小数
@@ -37,12 +24,34 @@ const computeTime = function(createdAt){
      // 超过一年
     res = `${(duration / 60 / 24 / 30 / 365).toFixed(0)}年前`
   }
-  return res
+  return res;
 }
 
+const article = await useFetch('/api/article');
+const {data:articleArr} = await useFetch('/api/articles')
+// console.log(articleArr)
+const articles = ref([]);
+articles = articleArr.value.data;
+// console.log(articles)
+articles.forEach((ele)=>{
+  ele.createdAt = computeTime(ele.createdAt);
+  console.log(ele.createdAt)
+  ele.img.url = `http://localhost:1337${ele.img.url}`
+})
+// console.log(articles)
 
-const time = computeTime(createdAt);
+const display = article.data.value.summary;
+const tittle = article.data.value.tittle;
+const creatAt = article.data.value.createdAt;
+const src = `http://localhost:1337${article.data.value.img.url}`
+const good = article.data.value.good;
+const discuss = article.data.value.discuss;
+const see = article.data.value.see;
+const authorName = article.data.value.author.name;
+const tagArr = article.data.value.tags.data;
 
+
+const time = computeTime(creatAt);
 
 useHead({
   title:'掘金',
@@ -110,36 +119,69 @@ useHead({
                 </router-link>
               </div>
             </div>
-            <div class="content-body">
+            <div class="content-body" v-for="(item,index) in articles" :key="index">
               <div class="list-tab">
-                <span class="tad-write">kokomi | 1天前 | 前端</span>
+                <span class="tad-write">{{item.author.name}} | {{item.createdAt}}  <span v-for="(item,index) in item.tags.data" :key="index">| {{ item.tittle }}</span></span>
               </div>
               <div class="main-content">
                 <!--TODO:文章列表-->
                 <router-link to="/detail" class="tittle" target="_blank">
-                  计算机网络模型
+                  {{item.tittle}}
                 </router-link>
                 <div class="head-content">
                    <router-link to="/detail" class="test" target="_blank">
-                    作为一名开发工程师，大部分时间都要与计算机打交道，但是现在依然有很多同学...
+                    {{item.summary}}
                   </router-link>
                   <div class="bottom-tab">
                     <div class="bottom-see">
                       <svg t="1677457805402" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1947" width="16" height="16"><path d="M515.2 224c-307.2 0-492.8 313.6-492.8 313.6s214.4 304 492.8 304 492.8-304 492.8-304S822.4 224 515.2 224zM832 652.8c-102.4 86.4-211.2 140.8-320 140.8s-217.6-51.2-320-140.8c-35.2-32-70.4-64-99.2-99.2-6.4-6.4-9.6-12.8-16-19.2 3.2-6.4 9.6-12.8 12.8-19.2 25.6-35.2 57.6-70.4 92.8-102.4 99.2-89.6 208-144 329.6-144s230.4 54.4 329.6 144c35.2 32 64 67.2 92.8 102.4 3.2 6.4 9.6 12.8 12.8 19.2-3.2 6.4-9.6 12.8-16 19.2C902.4 585.6 870.4 620.8 832 652.8z" p-id="1948"></path><path d="M512 345.6c-96 0-169.6 76.8-169.6 169.6 0 96 76.8 169.6 169.6 169.6 96 0 169.6-76.8 169.6-169.6C681.6 422.4 604.8 345.6 512 345.6zM512 640c-67.2 0-121.6-54.4-121.6-121.6 0-67.2 54.4-121.6 121.6-121.6 67.2 0 121.6 54.4 121.6 121.6C633.6 582.4 579.2 640 512 640z" p-id="1949"></path></svg>
-                      <span>112</span>
+                      <span>{{item.see}}</span>
                     </div>
                     <div class="bottom-good">
                       <svg t="1677458374711" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3054" width="16" height="16"><path d="M857.28 344.992h-264.832c12.576-44.256 18.944-83.584 18.944-118.208 0-78.56-71.808-153.792-140.544-143.808-60.608 8.8-89.536 59.904-89.536 125.536v59.296c0 76.064-58.208 140.928-132.224 148.064l-117.728-0.192A67.36 67.36 0 0 0 64 483.04V872c0 37.216 30.144 67.36 67.36 67.36h652.192a102.72 102.72 0 0 0 100.928-83.584l73.728-388.96a102.72 102.72 0 0 0-100.928-121.824zM128 872V483.04c0-1.856 1.504-3.36 3.36-3.36H208v395.68H131.36A3.36 3.36 0 0 1 128 872z m767.328-417.088l-73.728 388.96a38.72 38.72 0 0 1-38.048 31.488H272V476.864a213.312 213.312 0 0 0 173.312-209.088V208.512c0-37.568 12.064-58.912 34.72-62.176 27.04-3.936 67.36 38.336 67.36 80.48 0 37.312-9.504 84-28.864 139.712a32 32 0 0 0 30.24 42.496h308.512a38.72 38.72 0 0 1 38.048 45.888z" p-id="3055" fill="#707070"></path></svg>
-                      <span>112</span>
+                      <span>{{item.good}}</span>
                     </div>
                     <div class="bottom-good">
                       <svg t="1677458452890" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8724" width="16" height="16"><path d="M181.950464 904.356864c-10.888192 0-21.697536-0.336896-32.377856-0.99328l-48.2304-2.963456 42.43968-22.79936c50.363392-27.060224 90.082304-70.546432 112.79872-123.194368C156.571648 689.539072 99.479552 593.496064 99.479552 489.30816c0-187.336704 188.312576-339.760128 419.76832-339.760128S938.998784 301.971456 938.998784 489.30816c0 187.341824-188.294144 339.760128-419.750912 339.760128-18.069504 0-36.352-0.960512-54.388736-2.854912-42.69568 26.135552-90.272768 46.212096-141.495296 59.675648C276.752384 898.148352 229.175296 904.356864 181.950464 904.356864zM519.248896 177.015808c-216.107008 0-391.93088 140.096512-391.93088 312.292352 0 97.857536 56.060928 188.228608 153.796608 247.955456l9.73312 5.958656-4.037632 10.551296c-18.650112 48.950272-50.323456 91.405312-91.144192 122.9312 40.2944-1.074176 80.775168-6.902784 120.51968-17.350656 50.148352-13.182976 96.55808-33.00352 137.920512-58.91584l4.174848-2.598912 4.891648 0.562176c18.571264 2.132992 37.436416 3.204096 56.076288 3.204096 216.09472 0 391.907328-140.091392 391.907328-312.297472C911.156224 317.11232 735.342592 177.015808 519.248896 177.015808z" fill="#272636" p-id="8725"></path></svg>
-                      <span>112</span>
+                      <span>{{ item.discuss }}</span>
                     </div>  
                   </div>
                 </div>
                 <router-link to="/detail" class="main-img" target="_blank">
-                  <img src="https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1e805d92aaf544c386307fb00bb63c44~tplv-k3u1fbpfcp-no-mark:240:240:240:160.awebp?" width="120" height="80" />
+                  <img :src="item.img.url" width="120" height="80" />
+                </router-link>
+              </div>
+            </div>
+            <div class="content-body" v-for="(item,index) in articles" :key="index">
+              <div class="list-tab">
+                <span class="tad-write">{{item.author.name}} | {{item.createdAt}}  <span v-for="(item,index) in item.tags.data" :key="index">| {{ item.tittle }}</span></span>
+              </div>
+              <div class="main-content">
+                <!--TODO:文章列表-->
+                <router-link to="/detail" class="tittle" target="_blank">
+                  {{item.tittle}}
+                </router-link>
+                <div class="head-content">
+                   <router-link to="/detail" class="test" target="_blank">
+                    {{item.summary}}
+                  </router-link>
+                  <div class="bottom-tab">
+                    <div class="bottom-see">
+                      <svg t="1677457805402" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1947" width="16" height="16"><path d="M515.2 224c-307.2 0-492.8 313.6-492.8 313.6s214.4 304 492.8 304 492.8-304 492.8-304S822.4 224 515.2 224zM832 652.8c-102.4 86.4-211.2 140.8-320 140.8s-217.6-51.2-320-140.8c-35.2-32-70.4-64-99.2-99.2-6.4-6.4-9.6-12.8-16-19.2 3.2-6.4 9.6-12.8 12.8-19.2 25.6-35.2 57.6-70.4 92.8-102.4 99.2-89.6 208-144 329.6-144s230.4 54.4 329.6 144c35.2 32 64 67.2 92.8 102.4 3.2 6.4 9.6 12.8 12.8 19.2-3.2 6.4-9.6 12.8-16 19.2C902.4 585.6 870.4 620.8 832 652.8z" p-id="1948"></path><path d="M512 345.6c-96 0-169.6 76.8-169.6 169.6 0 96 76.8 169.6 169.6 169.6 96 0 169.6-76.8 169.6-169.6C681.6 422.4 604.8 345.6 512 345.6zM512 640c-67.2 0-121.6-54.4-121.6-121.6 0-67.2 54.4-121.6 121.6-121.6 67.2 0 121.6 54.4 121.6 121.6C633.6 582.4 579.2 640 512 640z" p-id="1949"></path></svg>
+                      <span>{{item.see}}</span>
+                    </div>
+                    <div class="bottom-good">
+                      <svg t="1677458374711" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3054" width="16" height="16"><path d="M857.28 344.992h-264.832c12.576-44.256 18.944-83.584 18.944-118.208 0-78.56-71.808-153.792-140.544-143.808-60.608 8.8-89.536 59.904-89.536 125.536v59.296c0 76.064-58.208 140.928-132.224 148.064l-117.728-0.192A67.36 67.36 0 0 0 64 483.04V872c0 37.216 30.144 67.36 67.36 67.36h652.192a102.72 102.72 0 0 0 100.928-83.584l73.728-388.96a102.72 102.72 0 0 0-100.928-121.824zM128 872V483.04c0-1.856 1.504-3.36 3.36-3.36H208v395.68H131.36A3.36 3.36 0 0 1 128 872z m767.328-417.088l-73.728 388.96a38.72 38.72 0 0 1-38.048 31.488H272V476.864a213.312 213.312 0 0 0 173.312-209.088V208.512c0-37.568 12.064-58.912 34.72-62.176 27.04-3.936 67.36 38.336 67.36 80.48 0 37.312-9.504 84-28.864 139.712a32 32 0 0 0 30.24 42.496h308.512a38.72 38.72 0 0 1 38.048 45.888z" p-id="3055" fill="#707070"></path></svg>
+                      <span>{{item.good}}</span>
+                    </div>
+                    <div class="bottom-good">
+                      <svg t="1677458452890" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8724" width="16" height="16"><path d="M181.950464 904.356864c-10.888192 0-21.697536-0.336896-32.377856-0.99328l-48.2304-2.963456 42.43968-22.79936c50.363392-27.060224 90.082304-70.546432 112.79872-123.194368C156.571648 689.539072 99.479552 593.496064 99.479552 489.30816c0-187.336704 188.312576-339.760128 419.76832-339.760128S938.998784 301.971456 938.998784 489.30816c0 187.341824-188.294144 339.760128-419.750912 339.760128-18.069504 0-36.352-0.960512-54.388736-2.854912-42.69568 26.135552-90.272768 46.212096-141.495296 59.675648C276.752384 898.148352 229.175296 904.356864 181.950464 904.356864zM519.248896 177.015808c-216.107008 0-391.93088 140.096512-391.93088 312.292352 0 97.857536 56.060928 188.228608 153.796608 247.955456l9.73312 5.958656-4.037632 10.551296c-18.650112 48.950272-50.323456 91.405312-91.144192 122.9312 40.2944-1.074176 80.775168-6.902784 120.51968-17.350656 50.148352-13.182976 96.55808-33.00352 137.920512-58.91584l4.174848-2.598912 4.891648 0.562176c18.571264 2.132992 37.436416 3.204096 56.076288 3.204096 216.09472 0 391.907328-140.091392 391.907328-312.297472C911.156224 317.11232 735.342592 177.015808 519.248896 177.015808z" fill="#272636" p-id="8725"></path></svg>
+                      <span>{{ item.discuss }}</span>
+                    </div>  
+                  </div>
+                </div>
+                <router-link to="/detail" class="main-img" target="_blank">
+                  <img :src="item.img.url" width="120" height="80" />
                 </router-link>
               </div>
             </div>
@@ -163,9 +205,7 @@ useHead({
               晚上好
             </div>
           </div>
-        </div>
-      </div>
-      <div class="toTop">
+          <div class="toTop">
         <!--TODO:backtotop-->
         <i>
           <svg data-v-0b2d23d8="" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
@@ -180,7 +220,10 @@ useHead({
             </path>
           </svg>
         </i>
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -203,25 +246,25 @@ useHead({
   }
   .body{
     background-color: #f4f5f5;
-    height: 6000px;
+    height: auto;
     padding: 0 286px 0 286px;
   }
   .main{
-    width: 960px;;
+    width: 960px;
     padding-top: 15px;
     display: flex;
     .content{
       background-color: white;
       width: 700px;
-      height: 1000px;
+      height: auto;
       .content-nav{
         height: 15px;
         padding: 15px 12px 15px 12px;
         border-bottom: 1px solid #f4f4f4;
-        display: flex;;
+        display: flex;
   
         div{
-          font-size: 14px;;
+          font-size: 14px;
           color: #5e6d82;
           padding: 0 14.5px 0 14.5px;
           border-right: 1px solid #f4f4f4;
@@ -278,23 +321,7 @@ useHead({
       }
     //}
   }
-  .toTop{
-      position: fixed;
-      bottom: 25px;
-      right: 25px;
-      background-color: #fff;
-      width: 38px;
-      height: 38px;
-      border-radius: 50%;
-      color: #409eff;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 20px;
-      box-shadow: 0 0 6px rgb(0 0 0 / 12%);
-      cursor: pointer;
-      z-index: 5;
-  }
+
   .nav-bottom{
       height: 46px;
       width: 100%;
@@ -381,6 +408,23 @@ useHead({
       }
     }
   }
+}
+.toTop{
+  position: fixed;
+  bottom: 25px;
+  right: 25px;
+  background-color: #fff;
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+  color: #409eff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  box-shadow: 0 0 6px rgb(0 0 0 / 12%);
+  cursor: pointer;
+  z-index: 5;
 }
 .list-tab{
   width: 660px;
---
+++ src/server/api/articles.js
@@ -0,0 +1,63 @@
+import { useGraphql } from '../../utils/useGraph'
+
+export default defineEventHandler(async () => {
+  const reqQuery = `query {
+    articles{
+      data{
+        attributes{
+          tittle,
+          content,
+          createdAt,
+          good,
+          discuss,
+          see,
+          summary,
+          tags{
+            data{
+              attributes{
+                tittle
+              }
+            }
+          },
+          author{
+            data{
+              attributes{
+                name,
+                discuss,
+                level{
+                  data{
+                    attributes{
+                      url
+                    }
+                  }
+                },
+                badge{
+                  data{
+                    attributes{
+                      url
+                    }
+                  }
+                },
+                headImg{
+                  data{
+                    attributes{
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          },
+          img{
+            data{
+              attributes{
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+  return (await useGraphql(reqQuery)).articles;
+})
